6db7107f48ae525083eb0983d3203bf265661908
Jinsol <jinsol0323@naver.com>
2023-03-04 23:57:26 +0900

    src_Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import Customer from "./components/Customer";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"></img>
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customer = {
+  id: 1,
+  image: "https://placeimg.com/64.64/any",
+  name: "김레오",
+  birthday: "961222",
+  gender: "남자",
+  job: "대학생",
+};
+
+class App extends Component {
+  render() {
+    return (
+      <Customer
+        id={customer.id}
+        image={customer.image}
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
+    );
+  }
 }
 
 export default App;
